e798429d54d6579456ebe8c947091ba3dc5fd343
Alberto Cavalin <acavalin@users.noreply.github.com>
2025-10-02 22:12:00 +0200

    minor changes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-# makeself --nocrc --nomd5 --tar-quietly --xz . ~/bin/dsp-w215.run dsp-w215-hnap run.sh
+# makeself --nocrc --nomd5 --tar-quietly --xz . ~/bin/dsp-w215.run dsp-w215-hnap ./run.sh
 exec node app.js "$@"
